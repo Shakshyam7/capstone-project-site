9ed663e1ac5f3058b7334bebfa0a21d1f7f207de
Shakshyam7 <90713152+Shakshyam7@users.noreply.github.com>
2023-02-26 19:34:21 -0800

    Card Component
    Used the map method to render the different images in the card component

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -2,16 +2,17 @@ import React, { useState } from "react";
 import { MdFavoriteBorder } from "react-icons/md";
 import "./Card.css";
 
-function Card(props) {
+function Card({image, alt, title }) {
   const [isHovered, setIsHovered] = useState(false);
   const handleHover = () => setIsHovered(!isHovered);
+
   return (
     <>
       <div className="card__container">
         <img className="card__image"
-          src={props.Image}
-          alt={props.Alt}
-          width=" 350px"
+          src={image}
+          alt={alt}
+          width=" 300px"
           height="378px"
           onMouseEnter={() => handleHover()}
           onMouseLeave={() => handleHover()}
@@ -21,6 +22,9 @@ function Card(props) {
             <MdFavoriteBorder />
           </div>
         )}
+         {isHovered && (
+          <p className="title">{title}</p>
+          )} 
       </div>
     </>
   );
--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,29 @@
 import React from "react";
-import HeroSection from "./Components/HeroSection";
+
 import "./Home.css";
-import Footer from "./Components/Footer";
+import HeroSection from "./Components/HeroSection";
+import Card from "./Components/Card/Card";
+import Flower from "./Images/ful.jpg";
+import Hilo from "./Images/hilo.jpg";
+import Moon from "./Images/moon.jpg"
+
+const cardItems = [
+  {
+    image: Flower,
+    alt: "Paining of Flower hanging int the Wall",
+    title: "Flower by my side",
+  },
+  {
+    image: Hilo,
+    alt: "Paining of Flower hanging int the Wall",
+    title: "Flower by my side",
+  },
+  {
+    image: Moon,
+    alt: "Paining of Flower hanging int the Wall",
+    title: "Flower by my side",
+  },
+];
 
 function Home() {
   return (
@@ -12,6 +34,13 @@ function Home() {
         }
       />
       <h2 className="text">Explore Arts from Creative and talented Artists</h2>
+      <div className="cardContainer">
+         { cardItems.map((item, index) => (
+        <Card key = {index} image = {item.image}
+        title = {item.title} alt = {item.alt}
+         />))}
+    
+      </div>
     </div>
   );
 }
