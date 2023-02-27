521109fbe654f2e8d379271a9264d4b108f29637
Shakshyam7 <90713152+Shakshyam7@users.noreply.github.com>
2023-02-26 22:18:05 -0800

    Sub title
    Adding a sub title that will link to contact us page

--- src/Home.js
+++ src/Home.js
@@ -5,10 +5,11 @@ import HeroSection from "./Components/HeroSection";
 import Card from "./Components/Card/Card";
 import Flower from "./Images/ful.jpg";
 import Hilo from "./Images/hilo.jpg";
-import Serenity from "./Images/serenity.jpeg"
-import Pond from "./Images/pond.jpeg"
-import RedForest from "./Images/Red Forest.jpeg"
-import Ship from "./Images/ship.jpg"
+import Serenity from "./Images/serenity.jpeg";
+import Pond from "./Images/pond.jpeg";
+import RedForest from "./Images/Red Forest.jpeg";
+import Ship from "./Images/ship.jpg";
+import Button from "./Components/Button";
 
 const cardItems = [
   {
@@ -51,13 +52,25 @@ function Home() {
           "Add a touch of Beauty to your Home with our Stunning Pieces of Art."
         }
       />
-      <heading className="text">Explore Arts from Creative and talented Artists</heading>
+      <heading className="text">
+        Explore Arts from Creative and talented Artists
+      </heading>
       <div className="cardContainer">
-         { cardItems.map((item, index) => (
-        <Card key = {index} image = {item.image}
-        title = {item.title} alt = {item.alt}
-         />))}
-    
+        {cardItems.map((item, index) => (
+          <Card
+            key={index}
+            image={item.image}
+            title={item.title}
+            alt={item.alt}
+          />
+        ))}
+        <div className="text2">
+          <div>
+            We make customized art for You and your house abd ship to your house
+            with out any fees.
+          </div>
+          <Button class = {"button__primary"} name = {"Contact Us"}  />
+        </div>
       </div>
     </div>
   );
