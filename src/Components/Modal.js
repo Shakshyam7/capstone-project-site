b8661929d3d7e8e1ea644fb71ba4f2df972e20e9
Shakshyam7 <90713152+Shakshyam7@users.noreply.github.com>
2023-02-27 19:23:16 -0800

    Modal
    Adjusting styles of the modal added the close icon of the model

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,25 +1,58 @@
 import React from "react";
+import { useState } from "react";
 import "./Modal.css";
 
 import RedForest from "./../Images/RedForest.jpeg";
 import Me from "./../Images/Me.jpg";
 import Button from "./Button";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import  { HiOutlineX } from "react-icons/hi";
+
 
 function Modal() {
+  const [click, setClick] = useState(false);
+
   return (
     <>
       <div className="modal">
         <div className="modalContainer">
-          <div className="image-profileContainer">
-            <img src={RedForest} width="80%" height="70%" />
-            <div className="profileContainer">
-              <img src={Me} className="profileImage"></img>
-              <p className="profileName">Shakshyam Sapkota</p>
+          <div className="closeIcon"><HiOutlineX /> </div>
+          <div className="modalItemsContainer">
+            <div className="image-profileContainer">
+              <img
+                className="modalImage"
+                src={RedForest}
+                width="80%"
+                height="70%"
+                alt="A wall art of forest in red color"
+              />
+              <div className="profileContainer">
+                <img src={Me} className="profileImage"></img>
+                <p className="profileName">Shakshyam Sapkota</p>
+              </div>
             </div>
-          </div>
-          <div className="modaltextContainer">
-            <p className="modelTitle">Walking in Red Forest</p>
-            <Button class="button__primary" name={"Add to Cart"} />
+            <div className="modaltextContainer">
+              <p className="modelTitle">Walking in Red Forest</p>
+              <p className="modelDescription">Price: $900</p>
+
+              <div className="buttonsContainer">
+                <div>
+                  <Button class="button__primary" name={"Add to Cart"} />
+                </div>
+                <div className="favButton" onClick={() => setClick(!click)}>
+                  {click ? (
+                    <FavoriteIcon
+                      sx={{ fontSize: 50, color: "rgba(209, 81, 111, 0.87)" }}
+                    />
+                  ) : (
+                    <FavoriteBorderIcon
+                      sx={{ fontSize: 50, color: ")" }}
+                    />
+                  )}
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
