0e8699d06d9f4abd8268586df8a3e2e6b219ac2a
Shakshyam7 <90713152+Shakshyam7@users.noreply.github.com>
2023-02-27 16:55:52 -0800

    Profile Container
    Creating a profile container beside the image

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -3,15 +3,18 @@ import "./Modal.css";
 
 
 import RedForest from "./../Images/RedForest.jpeg"
+import Me from "./../Images/Me.jpg" 
 
 function Modal() {
   return (
     <>
       <div className="modal">
         <div className="modalContainer">
-          <img src = {RedForest}width = "60%"height= "60%" />
-          <h2>Heading</h2>
+          <img src = {RedForest}width = "60%"height= "40%" />
+          <div className="profileContainer">
+          <img src = {Me} className="profileImage"></img>
           <p>This is a modal.</p>
+          </div>
         </div>
       </div>
     </>
