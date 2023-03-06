5c1c883d33926ebea9df024e542156bac7e83c53
Shakshyam7 <90713152+Shakshyam7@users.noreply.github.com>
2023-03-05 22:38:04 -0800

    styling newsletter
    Adjusting the styles of the component

--- src/Components/Newsletter.js
+++ src/Components/Newsletter.js
@@ -6,12 +6,14 @@ import Button from "./Button";
 function Newsletter() {
   return (
     <div className="ncontainer">
-        <div className = "ntitle">Newsletter</div>
-        <div className = "ntext">Leave your email to receive latest arts update from our talented artist </div>
-        <div>       
-        <Textfield class = {"textfield"} type = {""} placeholder = {"Email"}/>
-        <Button class = "button__primary" name = "Send"/>
-        </div>
+      <div className="ntitle">Newsletter</div>
+      <div className="ntext">
+        Leave your email to receive latest arts update from our talented artist{" "}
+      </div>
+      <div>
+        <Textfield class={"textfield"} type={""} placeholder={"Email"} />
+        <Button class="button__primary" name="Send" />
+      </div>
     </div>
   );
 }
