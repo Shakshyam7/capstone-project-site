75f408d938aa6250c42afd91fc93dfc9f7d986f1
Shakshyam7 <90713152+Shakshyam7@users.noreply.github.com>
2023-03-05 22:13:29 -0800

    Adding component
    Adding textfield and button component to the news letter

--- src/Components/Newsletter.js
+++ src/Components/Newsletter.js
@@ -1,13 +1,17 @@
 import React from "react";
 import "./Newsletter.css";
 import Textfield from "./textfield";
+import Button from "./Button";
 
 function Newsletter() {
   return (
-    <div>
+    <div className="ncontainer">
         <div className = "ntitle">Newsletter</div>
         <div className = "ntext">Leave your email to receive latest arts update from our talented artist </div>
+        <div>       
         <Textfield class = {"textfield"} type = {""} placeholder = {"Email"}/>
+        <Button class = "button__primary" name = "Send"/>
+        </div>
     </div>
   );
 }
