754ee05e13e3bf78e2c795fab2d67bd909655988
Shakshyam7 <90713152+Shakshyam7@users.noreply.github.com>
2023-03-06 19:29:23 -0800

    Adding id to cardItems.json file and impoeting image in contact. page from public/images

--- src/Contact.js
+++ src/Contact.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 import AppForm from "./Components/AppForm";
-import contact from "./images/contact.jpg";
 
 import "./Contact.css";
 
@@ -9,18 +8,25 @@ function Contact() {
   return (
     <>
       <div className="imageContainer">
-        <img src={contact} width="50%" alt = "A lady answering calls and emails " />
+        <img
+          src={"images/contact.jpg"}
+          width="50%"
+          alt="A lady answering calls and emails "
+        />
       </div>
       <div className="appFormContainer">
         <div className="contactText">
-           <cont className="cont">Contact Us</cont>
+          <cont className="cont">Contact Us</cont>
           <p className="para"> We would love to speak with you. </p>
-          <p className="para"> Feel free to reach out using the below details.</p>
-          <p className = "tit">Get In touch</p>
+          <p className="para">
+            {" "}
+            Feel free to reach out using the below details.
+          </p>
+          <p className="tit">Get In touch</p>
           <p className="para"> Phone: +17783475839</p>
         </div>
 
-          <AppForm />
+        <AppForm />
       </div>
     </>
   );
