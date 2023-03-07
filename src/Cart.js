c4bb49a9e342133aaa873ed77b5439692d4effe5
Shakshyam7 <90713152+Shakshyam7@users.noreply.github.com>
2023-03-07 01:58:23 -0800

    Adding Continue Shopping button when the cart does not have anything

--- src/Cart.js
+++ src/Cart.js
@@ -2,28 +2,42 @@ import React from "react";
 import "./Cart.css";
 
 import CartProducts from "./Components/CartProducts";
-import { CartProvider, useCart } from "react-use-cart";
+import { useCart } from "react-use-cart";
+import Button from "./Components/Button";
 
 function Cart() {
-  const { items, isEmpty, removeItem } =
-    useCart();
+  const { items, isEmpty, removeItem } = useCart();
 
   return (
     <>
       <div className="cartContainer">
         {isEmpty ? (
-          <p className = "cartPageTitle">Your cart is empty</p>
-        ) : (
-          items.map((item) => (
+          <>
+            <div className="emptyPage">
+              <div className="cartPageTitle">Your cart is empty</div>
+                <Button
+                  className={"button__primary"}
+                  name={"Continue Shopping"}
+                />
+            </div>
+          </>
+        ) : 
+        (<>
+
+         { items.map((item) => (
             <CartProducts
               key={item.id}
               image={item.image}
               title={item.title}
               price={item.price}
+              alt = {item.alt}
               onClick={() => removeItem(item.id)}
             />
           ))
-        )}
+         }
+        </> )}
+
+        
       </div>
     </>
   );
