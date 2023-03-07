0da3167491a248444a7ec2a636fc6983b8542b99
Shakshyam7 <90713152+Shakshyam7@users.noreply.github.com>
2023-03-06 20:01:59 -0800

    adding state to to pass the data from the card to the modal using useState  hook

--- src/Shop.js
+++ src/Shop.js
@@ -1,36 +1,43 @@
-import React from 'react'
-import Card from './Components/Card/Card'
-import "./Shop.css"
-import shopItems from"./shopItems.json"
-import { useState } from 'react'
-import Modal from './Components/Modal'
-
+import React from "react";
+import Card from "./Components/Card/Card";
+import "./Shop.css";
+import shopItems from "./shopItems.json";
+import { useState } from "react";
+import Modal from "./Components/Modal";
 
 function Shop() {
   const [showContent, setShowContent] = useState(false);
-  const handleClick = () => {
+  const [selectedItem, setSelectedItem] = useState(null);
+  const handleClick = (item) => {
     setShowContent(!showContent);
+    setSelectedItem(item);
   };
   return (
     <>
-    <div className='shopContainer'>
-      {shopItems.map((item, index) => (
-        <Card 
-        key={index}
-        image={item.image}
-        title={item.title}
-        alt={item.alt}
-        onClick = {()=>handleClick()}
-        />
-      ))}
-      {showContent && <Modal />}
-
-      
-    
-    </div>
+      <div className="shopContainer">
+        {shopItems.map((item, index) => (
+          <Card
+            key={index}
+            image={item.image}
+            title={item.title}
+            alt={item.alt}
+            onClick={() => handleClick(item)}
+          />
+        ))}
+        {showContent && (
+          <Modal
+            image={selectedItem.image}
+            title={selectedItem.title}
+            alt={selectedItem.alt}
+            price={selectedItem.price}
+            artist={selectedItem.artist}
+            artistImage={selectedItem.artistImage}
+            onClick={() => setShowContent(false)}
+          />
+        )}
+      </div>
     </>
-      
-  )
+  );
 }
 
-export default Shop
+export default Shop;
