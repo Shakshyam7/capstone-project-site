206e6783218e20f6707205f934082f50782651cf
Shakshyam7 <90713152+Shakshyam7@users.noreply.github.com>
2023-03-06 19:51:49 -0800

    Rendering the data in the shop page using card component

--- src/Shop.js
+++ src/Shop.js
@@ -1,11 +1,22 @@
 import React from 'react'
+import Card from './Components/Card/Card'
 import "./Shop.css"
+import shopItems from"./shopItems.json"
 
 
 function Shop() {
   return (
     <>
     <div className='shopContainer'>
+      {shopItems.map((item, index) => (
+        <Card 
+        key={index}
+        image={item.image}
+        title={item.title}
+        alt={item.alt}
+        />
+      ))}
+
       
     
     </div>
