d0cc613d69d5371c4fab0b86671b8fee57b66700
Shakshyam7 <90713152+Shakshyam7@users.noreply.github.com>
2023-03-17 02:42:57 -0700

    Adjusting styles and adding the state to change the logo for the card

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,15 +1,24 @@
 import React from "react";
-import { MdFavoriteBorder } from "react-icons/md";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 import "./Card.css";
+import { useState } from "react";
 
 function Card({ image, alt, title, onClick }) {
+  const [click, setClick] = useState(false);
   return (
     <>
       <div className="card__container" onClick={onClick}>
         <img className="card__image" src={image} alt={alt} />
 
-        <div className="wishlist__logo">
-          <MdFavoriteBorder />
+        <div className="wishlist__logo" onClick={() => setClick(!click)}>
+          {click ? (
+            <FavoriteIcon
+              sx={{ fontSize: 40, color: "rgba(209, 81, 111, 0.87)" }}
+            />
+          ) : (
+            <FavoriteBorderIcon sx={{ fontSize: 40, color: "#212529" }} />
+          )}
         </div>
 
         <p className="title">{title}</p>
