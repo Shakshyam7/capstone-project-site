0e67b7ef5c114c3face9fdecc029317697fb85ef
Shakshyam7 <90713152+Shakshyam7@users.noreply.github.com>
2023-03-30 02:04:53 -0700

    Adding the social icons to the header component

--- src/DesignSystemNav/DesignSystemNav.js
+++ src/DesignSystemNav/DesignSystemNav.js
@@ -2,15 +2,28 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import "./DesignSystemNav.css";
-
+import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
 
 function DesignSystemNav() {
   return (
     <>
       <div className="dnavbar-container">
         <div className="dtitle">Design System</div>
-        <div className="site-linkContainer"> 
-          <Link to="/home" className="site-link">Go to Site</Link>
+        <div className="site-linkContainer">
+          <Link to="/home" className="site-link">
+            Go to Site
+          </Link>
+        </div>
+        <div className="socialContainer">
+          <div className="githubIcon">
+            <FaGithub />
+          </div>
+          <div className="linkedInIcon">
+            <FaLinkedin />
+          </div>
+          <div className="instaIcon">
+            <FaInstagram />
+          </div>
         </div>
       </div>
     </>
