1860c071cf3c11ac20555da3f37f2f914128cd6a
Shakshyam7 <90713152+Shakshyam7@users.noreply.github.com>
2023-03-30 01:56:49 -0700

    Adjusting styles of the design system navbar

--- src/DesignSystemNav/DesignSystemNav.js
+++ src/DesignSystemNav/DesignSystemNav.js
@@ -1,12 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./DesignSystemNav.css";
+
 
 function DesignSystemNav() {
   return (
     <>
       <div className="dnavbar-container">
-        <div className="dtitle">ArtShop Design System</div>
+        <div className="dtitle">Design System</div>
+        <div className="site-linkContainer"> 
+          <Link to="/home" className="site-link">Go to Site</Link>
+        </div>
       </div>
     </>
   );
